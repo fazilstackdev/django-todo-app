d67847d8819d3e27485d39c1ea46783881b3f476
fazil <fazilahmadi755@gmail.com>
2026-01-06 09:47:37 +0430

    models_database_added

--- tasks/views.py
+++ tasks/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    return render(request,'tasks/list.html')
+
